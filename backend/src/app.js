5dbb9e94728b7c30e586a1dc7c37bb366595382e
naimatmunna <naimatullah.dev@gmail.com>
2026-07-09 12:23:14 +0500

    update cookies

--- backend/src/app.js
+++ backend/src/app.js
@@ -38,7 +38,8 @@ export const createApp = () => {
   );
   app.use(express.json({ limit: '1mb' }));
   app.use(express.urlencoded({ extended: true, limit: '1mb' }));
-  app.use(cookieParser(config.cookie.secret));
+  // app.use(cookieParser(config.cookie.secret));
+  app.use(cookieParser());
   app.use(compression());
   app.use(hpp());
   app.use(mongoSanitizer);
